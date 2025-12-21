f0e74c2a8bc5537396bd3b0c39e83434ed7e750b
Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
2025-12-21 18:58:23 +0100

    playing w/JSON

--- lib/MB.ml
+++ lib/MB.ml
@@ -21,10 +21,20 @@ let interpret_json json =
          (fun (name, subtree) ->
            Printf.printf "%s%s = \n" pfx name;
            interpret_json' (pfx ^ "  ") subtree ) assoc
-    | `List jsons -> List.iter (interpret_json' pfx) jsons
+    | `List jsons ->
+       Printf.printf "%s[\n" pfx;
+       List.iter (interpret_json' (pfx ^ "  ")) jsons;
+       Printf.printf "%s]\n" pfx;
     | `Null -> Printf.printf "%sNULL\n" pfx
     | `Bool bool -> Printf.printf "%s%b\n" pfx bool
     | `Int n -> Printf.printf "%s%d\n" pfx n
     | `Float x -> Printf.printf "%s%g\n" pfx x
     | `String s -> Printf.printf "%s%s\n" pfx s in
-  interpret_json' "" json
+  interpret_json' "" json;
+  Printf.printf "keys:\n";
+  List.iter print_endline (JSON.Util.keys json);
+  Printf.printf "id:\n";
+  interpret_json' "- " (JSON.Util.member "id" json);
+  Printf.printf "releases:\n";
+  interpret_json' "- " (JSON.Util.member "releases" json)
+
