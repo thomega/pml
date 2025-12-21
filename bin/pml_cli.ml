19d9e235412fc3334a46393f8bdf27385be06211
Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
2025-12-21 14:42:15 +0100

    options

--- bin/pml_cli.ml
+++ bin/pml_cli.ml
@@ -22,34 +22,67 @@ module Common : Common =
 
   end
 
-module type Unit_Cmd =
+module type Exit_Cmd =
   sig
-    val cmd : unit Cmd.t
+    val cmd : int Cmd.t
   end
 
-module Disc : Unit_Cmd =
+module Query_Disc : Exit_Cmd =
   struct
 
     let man = [
         `S Manpage.s_description;
         `P "Query the disc in the drive." ] @ Common.man_footer
 
+    let verbose =
+      let doc = "Be more verbose." in
+      Arg.(value & flag & info ["v"; "verbose"] ~doc)
+
+    let print_id =
+      let doc = "Print the discid as recognized by MusicBrainz." in
+      Arg.(value & flag & info ["i"; "id"] ~doc)
+
+    let print_freedb =
+      let doc = "Print the discid as recognized by FreeDB." in
+      Arg.(value & flag & info ["f"; "freedb"] ~doc)
+
+    let print_toc =
+      let doc = "Print the table of contents of the discid as used
+                 for fuzzy searches on MusicBrainz." in
+      Arg.(value & flag & info ["t"; "toc"] ~doc)
+
+    let query_disc _verbose id freedb toc =
+      match Pml.Discid.get () with
+      | Result.Ok ids ->
+         begin
+           if not id && not freedb && not toc then
+             Printf.printf "id = %s\nfreedb = %s\ntoc = %s\n" ids.id ids.freedb ids.toc
+           else if id then
+             print_endline ids.id
+           else if freedb then
+             print_endline ids.freedb
+           else if toc then
+             print_endline ids.toc
+         end;
+         0
+      | Result.Error msg ->
+         Printf.eprintf "error: %s!\n" msg;
+         1
+
     let term =
       let open Term in
-      const
-        (fun () ->
-          match Pml.Discid.get () with
-          | Result.Ok ids ->
-             Printf.printf "id = %s\nfreedb = %s\ntoc = %s\n" ids.id ids.freedb ids.toc
-          | Result.Error msg -> Printf.eprintf "error: %s!\n" msg)
-      $ const ()
+      const query_disc
+      $ verbose
+      $ print_id
+      $ print_freedb
+      $ print_toc
 
     let cmd =
-      Cmd.v (Cmd.info "disc" ~man) term
+      Cmd.(make (info "disc" ~man) term)
 
 end
 
-module Main : Unit_Cmd =
+module Main : Exit_Cmd =
   struct
 
     let _doc = "Physical Media Library"
@@ -63,14 +96,14 @@ module Main : Unit_Cmd =
         `P "Thorsten Ohl <ohl@physik.uni-wuerzburg.de>" ] @ Common.man_footer
 
     let cmd =
-      Cmd.group
-        (Cmd.info "pml" ~man)
-        [ Disc.cmd ]
+      let open Cmd in
+      group (info "pml" ~man)
+        [ Query_Disc.cmd ]
 
   end
 
 let main () =
-  Cmd.eval Main.cmd
+  Cmd.eval' Main.cmd
 
 let () =
   if !Sys.interactive then
