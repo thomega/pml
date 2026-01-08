3463e7c31be539fe43ab5e5ef983a9372ceb19a4
Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
2026-01-08 13:31:08 +0100

    Cache: functorized

--- lib/cache.ml
+++ lib/cache.ml
@@ -1,54 +1,3 @@
-let is_directory name =
-  if Sys.file_exists name then
-    if Sys.is_directory name then
-      Ok name
-    else
-      Error ("not a directory: " ^ name)
-  else
-    Error ("no such file or directory: " ^ name)
-    
-let dir ~root ~subdir =
-  match is_directory root with
-  | Error _ as e -> e
-  | Ok root -> is_directory (Filename.concat root subdir)
-
-let filename ~root ~subdir tag =
-  dir ~root ~subdir
-  |>  Result.map (fun path -> Filename.concat path tag)
-
-let lookup ~root ~subdir tag =
-  match filename ~root ~subdir tag with
-  | Error _ as e -> e
-  | Ok name ->
-     if Sys.file_exists name then
-       try
-         Ok (Some (In_channel.with_open_text name In_channel.input_all))
-       with
-       | exn -> Error (Printexc.to_string exn)
-     else
-       Ok None
-
-let delete ~root ~subdir tag =
-  match filename ~root ~subdir tag with
-  | Error _ as e -> e
-  | Ok name ->
-     if Sys.file_exists name then
-       try
-         Ok (Sys.remove name)
-       with
-       | exn -> Error (Printexc.to_string exn)
-     else
-       Ok ()
-
-let replace ~root ~subdir tag text =
-  match filename ~root ~subdir tag with
-  | Error _ as e -> e
-  | Ok name ->
-     try
-       Ok (Out_channel.with_open_text name (fun oc -> Out_channel.output_string oc text))
-     with
-     | exn -> Error (Printexc.to_string exn)
-
 module type T =
   sig
     val lookup : root:string -> string -> (string option, string) result
@@ -63,7 +12,56 @@ module type Table =
 
 module Make (Table : Table) : T =
   struct
-    let lookup ~root tag = lookup ~root ~subdir:Table.name tag
-    let delete ~root tag = delete ~root ~subdir:Table.name tag
-    let replace ~root text tag = replace ~root ~subdir:Table.name text tag
+
+    let is_directory name =
+      if Sys.file_exists name then
+        if Sys.is_directory name then
+          Ok name
+        else
+          Error ("not a directory: " ^ name)
+      else
+        Error ("no such file or directory: " ^ name)
+    
+    let table ~root =
+      match is_directory root with
+      | Error _ as e -> e
+      | Ok root -> is_directory (Filename.concat root Table.name)
+
+    let filename ~root tag =
+      table ~root
+      |>  Result.map (fun path -> Filename.concat path tag)
+
+    let lookup ~root tag =
+      match filename ~root tag with
+      | Error _ as e -> e
+      | Ok name ->
+         if Sys.file_exists name then
+           try
+             Ok (Some (In_channel.with_open_text name In_channel.input_all))
+           with
+           | exn -> Error (Printexc.to_string exn)
+         else
+           Ok None
+
+    let delete ~root tag =
+      match filename ~root tag with
+      | Error _ as e -> e
+      | Ok name ->
+         if Sys.file_exists name then
+           try
+             Ok (Sys.remove name)
+           with
+           | exn -> Error (Printexc.to_string exn)
+         else
+           Ok ()
+
+    let replace ~root tag text =
+      match filename ~root tag with
+      | Error _ as e -> e
+      | Ok name ->
+         try
+           Ok (Out_channel.with_open_text name (fun oc -> Out_channel.output_string oc text))
+         with
+         | exn -> Error (Printexc.to_string exn)
+
   end
