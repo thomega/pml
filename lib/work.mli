51838e67a7ce941f36e5bc6fed2444e3e6ed1509
Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
2026-01-12 07:53:52 +0100

    missing lib/work.mli

---
+++ lib/work.mli
@@ -0,0 +1,34 @@
+module Artist : sig
+  type t =
+    { id : string;
+      name : string;
+      sort_name : string option;
+      part : string option;
+      role : string option }
+  val of_mb : Musicbrainz.Artist.t -> t
+end
+
+module Track : sig
+  type t =
+    { id : string;
+      number : int;  (** Overall position of the track in the whole work, counting from 1. *)
+      title : string;
+      performers : Artist.t list }
+  val of_mb : Musicbrainz.Track.t -> t
+end
+
+module Partial : sig
+  type t =
+    { release : string; (** The Musicbrainz id of the release from which the data are taken. *)
+      disk : string; (** The diskid from which the audio was ripped. *)
+      title : string; (** The title of the whole work. This can not be empty. *)
+      composers : Artist.t list; (** Composers of a work that is expected to be performed
+                                     by others.  This will usually be left empty for
+                                     popular music.  *)
+      performers : Artist.t list; (** Instrumentalists, singers, conductors. *)
+      tracks : Track.t list;
+      total_tracks : int (** The total number of tracks of the piece.  This is only needed
+                             for the correct number of leading zeros in numbers in filenames. *)
+    }
+end
+(** The part of a a musical work that fits on one disk. *)
