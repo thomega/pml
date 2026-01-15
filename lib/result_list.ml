7389359580a4599e9ec364b10405780c42ac8e4c
Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
2026-01-15 22:03:02 +0100

    Result_list: use let-punning

--- lib/result_list.ml
+++ lib/result_list.ml
@@ -4,20 +4,23 @@ let cons a alist =
   let* a and* alist in
   Ok (a :: alist)
 
-let hd err = function
-  | Error _ as e -> e
-  | Ok [] -> Error err
-  | Ok (a :: _) -> Ok a
+let hd err alist =
+  let* alist in
+  match alist with
+  | [] -> Error err
+  | a :: _ -> Ok a
 
-let tl err = function
-  | Error _ as e -> e
-  | Ok [] -> Error err
-  | Ok (_ :: alist) -> Ok alist
+let tl err alist =
+  let* alist in
+  match alist with
+  | [] -> Error err
+  | _ :: alist -> Ok alist
 
-let rec map f = function
-  | Error _ as e -> e
-  | Ok [] -> Ok []
-  | Ok (a :: alist) ->
+let rec map f alist =
+  let* alist in
+  match alist with
+  | [] -> Ok []
+  | a :: alist ->
      let* fa = f a
      and* falist = map f (Ok alist) in
      Ok (fa :: falist)
