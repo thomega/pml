9f171e6df4a862cf8612deaf205f177ffbbff0e6
Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
2026-01-16 03:52:44 +0100

    Result_list: variants

--- lib/result_list.ml
+++ lib/result_list.ml
@@ -1,6 +1,10 @@
 open Result.Syntax
 
 let cons a alist =
+  let* alist in
+  Ok (a :: alist)
+
+let cons' a alist =
   let* a and* alist in
   Ok (a :: alist)
 
@@ -16,20 +20,36 @@ let tl err alist =
   | [] -> Error err
   | _ :: alist -> Ok alist
 
-let rec map f alist =
+let rec map f = function
+  | [] -> Ok []
+  | a :: alist ->
+     let* fa = f a
+     and* falist = map f alist in
+     Ok (fa :: falist)
+
+let%test _ =
+  map (fun i -> Ok (10 * i)) [] = Ok []
+
+let%test _ =
+  map (fun i -> Ok (10 * i)) [1] = Ok [10]
+
+let%test _ =
+  map (fun i -> Ok (10 * i)) [1; 2; 3] = Ok [10; 20; 30]
+
+let rec map' f alist =
   let* alist in
   match alist with
   | [] -> Ok []
   | a :: alist ->
      let* fa = f a
-     and* falist = map f (Ok alist) in
+     and* falist = map' f (Ok alist) in
      Ok (fa :: falist)
 
 let%test _ =
-  map (fun i -> Ok (10 * i)) (Ok []) = Ok []
+  map' (fun i -> Ok (10 * i)) (Ok []) = Ok []
 
 let%test _ =
-  map (fun i -> Ok (10 * i)) (Ok [1]) = Ok [10]
+  map' (fun i -> Ok (10 * i)) (Ok [1]) = Ok [10]
 
 let%test _ =
-  map (fun i -> Ok (10 * i)) (Ok [1; 2; 3]) = Ok [10; 20; 30]
+  map' (fun i -> Ok (10 * i)) (Ok [1; 2; 3]) = Ok [10; 20; 30]
