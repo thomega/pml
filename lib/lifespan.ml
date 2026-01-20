65ae7f0b36cec95cc821124aa4de2fb5f71c4801
Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
2026-01-20 14:58:08 +0100

    Musicbrainz.Lifespan -> Lifespan

---
+++ lib/lifespan.ml
@@ -0,0 +1,63 @@
+type t =
+  | Alive of Date.t
+  | Dead of Date.t * Date.t
+  | Dead' of Date.t
+  | Limbo
+
+let to_string = function
+  | Alive born -> "*" ^ Date.year_to_string born
+  | Dead (born, died) -> "*" ^ Date.year_to_string born ^ ", +" ^ Date.year_to_string died
+  | Dead' died -> "+" ^ Date.year_to_string died
+  | Limbo -> "*/+?"
+
+type relation =
+  | Before
+  | After
+  | Overlap
+
+let relation ls1 ls2 =
+  let open Date.Syntax in
+  match ls1, ls2 with
+  | Dead (b1, d1), Dead (b2, d2) ->
+     if d1 <= b2 then
+       Before
+     else if d2 <= b1 then
+       After
+     else
+       Overlap
+  | (Dead (_, d1) | Dead' d1), Alive b2 ->
+     if d1 <= b2 then
+       Before
+     else
+       Overlap
+  | Alive b1, (Dead (_, d2) | Dead' d2) ->
+     if d2 <= b1 then
+       After
+     else
+       Overlap
+  | Dead' d1, Dead (b2, _) ->
+     if d1 <= b2 then
+       Before
+     else
+       Overlap
+  | Dead (b1, _), Dead' d2 ->
+     if d2 <= b1 then
+       After
+     else
+       Overlap
+  | Limbo, _ | _, Limbo | Alive _, Alive _ | Dead' _, Dead' _ -> Overlap
+
+(* We have to deal with both optional and nullable strings. *)
+let make first last =
+  match Date.of_opt_string_opt (Option.join first),
+        Date.of_opt_string_opt (Option.join last) with
+  | Some born, Some died -> Dead (born, died)
+  | Some born, None -> Alive born
+  | None, Some died -> Dead' died
+  | None, None -> Limbo
+
+let jsont =
+  Jsont.Object.map ~kind:"Lifespan" make
+  |> Jsont.Object.opt_mem "begin" Jsont.(option string)
+  |> Jsont.Object.opt_mem "end" Jsont.(option string)
+  |> Jsont.Object.finish
---
+++ lib/lifespan.mli
@@ -0,0 +1,36 @@
+(** Birth and death of artists. *)
+
+type t =
+  | Alive of Date.t (** Known to be alive since. *)
+  | Dead of Date.t * Date.t (** Known to be have lived from/to. *)
+  | Dead' of Date.t (** Known to have lived until. *)
+  | Limbo (** Nothing known. *)
+
+(** We ignore the [ended] element.
+    {v
+     <element name="life-span">
+         <optional>
+             <element name="begin">
+                 <ref name="def_incomplete-date"/>
+             </element>
+         </optional>
+         <optional>
+             <element name="end">
+                 <ref name="def_incomplete-date"/>
+             </element>
+         </optional>
+         <optional>
+             <ref name="def_ended" />
+         </optional>
+     </element>
+     v} *)
+
+type relation = Before | After | Overlap
+(** If intervals are disjoint, we can assign composer
+    and performer roles unambigously. *)
+
+val relation : t -> t -> relation
+(** Check if intervals are disjoint. *)
+
+val jsont : t Jsont.t
+val to_string : t -> string
