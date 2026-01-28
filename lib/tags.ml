4850c349efc63ccf25514f790470759f46813a60
Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
2026-01-28 21:31:52 +0100

    more flexible cdparanoia

--- lib/tags.ml
+++ lib/tags.ml
@@ -446,24 +446,37 @@ module Disc =
       let* name = match_performer pfx d in
       user_performer name d
 
+    (** We use a prefix for the WAV file, because discids can start with
+        a period. *)
+    let wav_prefix = "cd-"
+
     let script d =
       let open Printf in
+      let wav_name i = sprintf "%s%s%02d.wav" wav_prefix d.discid i in
       let separator () =
         printf "########################################################################\n" in
       printf "#! /bin/sh\n";
       separator ();
-      printf "# tag disc %s\n" d.discid;
-      printf "#   a.k.a. %s\n" d.medium_id;
-      printf "#  release %s\n" d.release_id;
+      printf "DISCID='%s'\n" d.discid;
+      printf "MEDIUM='%s'\n" d.medium_id;
+      printf "RELEASE='%s'\n" d.release_id;
       separator ();
       printf "\n";
       separator ();
-      printf "# Rip CD unless an output file exists\n";
+      printf "# Rip CD track unless the output file exists\n";
       separator ();
       printf "\n";
-      printf "if [ ! -f track01.cdda.wav ]; then\n";
-      printf "  cdparanoia /dev/cdrom\n";
-      printf "fi\n";
+      printf "rip_track () {\n";
+      printf "  if [ ! -r $2 ]; then\n";
+      printf "    cdparanoia -w $1 $2\n";
+      printf "  fi\n";
+      printf "}\n";
+      printf "\n";
+      List.iter
+        (fun t ->
+          let n = t.Track.number_on_disc in
+          printf "rip_track %2d %s\n" n (wav_name n))
+        d.tracks;
       printf "\n";
       separator ();
       printf "# Set up target directory\n";
@@ -490,7 +503,7 @@ module Disc =
       printf "\n";
       List.iter
         (fun t ->
-          printf "WAV=track%02d.cdda.wav\n" t.Track.number_on_disc;
+          printf "WAV=%s\n" (wav_name t.Track.number_on_disc);
           printf "TITLE=\"%0*d %s\"\n" d.track_width t.Track.number t.Track.title;
           printf "\n")
         d.tracks;
