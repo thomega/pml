cb5d4815c76216b6ab1749949e3a39dba42f6811
Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
2026-01-29 16:27:30 +0100

    add GPL banner

--- lib/artist_type.ml
+++ lib/artist_type.ml
@@ -1,3 +1,20 @@
+(* artist_type.ml -- part of PML (Physical Media Library)
+
+  Copyright (C) 2026 by Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>. *)
+
 type voice =
   | Soprano
   | Mezzo
--- lib/option_syntax.ml
+++ lib/option_syntax.ml
@@ -1,3 +1,20 @@
+(* option_syntax.ml -- part of PML (Physical Media Library)
+
+  Copyright (C) 2026 by Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>. *)
+
 let product o1 o2 =
   match o1, o2 with
   | Some a1, Some a2 -> Some (a1, a2)
