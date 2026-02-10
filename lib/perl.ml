e25ee9f93020458cd9abe364d85c8d57825e1d82
Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
2026-02-10 16:15:56 +0100

    Perl: handle 'x' flag

--- lib/perl.ml
+++ lib/perl.ml
@@ -15,18 +15,21 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>. *)
 
-module CSet = Set.Make (Char)
+module CMap = Map.Make (Char)
 
-let cset_of_string s =
-  String.fold_right CSet.add s CSet.empty
+let lookup_flag ?(err_prefix="") ?(err_postfix="") map c =
+  CMap.find_opt c map
+  |> Option.to_result ~none:(err_prefix ^ Printf.sprintf "invalid flag '%c'" c ^ err_postfix)
 
-let all_chars_in cset s =
-  String.to_seq s |> Seq.exists (fun c -> not (CSet.mem c cset))
+let flags_of_string ?err_prefix ?err_postfix map s =
+  String.fold_right List.cons s []
+  |> Result_list.map (lookup_flag ?err_prefix ?err_postfix map)
 
 module M =
   struct
 
-    let allowed_flags = cset_of_string "i"
+    let flag_map =
+      CMap.of_list [('i', `CASELESS); ('x', `EXTENDED)]
 
     type t =
       { rex : Pcre2.regexp;
@@ -36,6 +39,7 @@ module M =
       p.text
 
     let myname = "Perl.M"
+    let err_prefix = myname ^ ": "
 
     let split_substitution s =
       let l = String.length s in
@@ -49,29 +53,22 @@ module M =
         | [rex; flags] -> Ok (rex, flags)
         | _ -> Error (Printf.sprintf "%s: too many '%c's in \"%s\"" myname delim s)
 
-    let%test _ = split_substitution "/ab" = Error ("Perl.M: missing second '/' in \"/ab\"")
+    let%test _ = split_substitution "/ab" = Error (err_prefix ^ {|missing second '/' in "/ab"|})
     let%test _ = split_substitution "/ab/" = Ok ("ab", "")
     let%test _ = split_substitution "/a/b" = Ok ("a", "b")
-    let%test _ = split_substitution "/a/b/" = Error ("Perl.M: too many '/'s in \"/a/b/\"")
+    let%test _ = split_substitution "/a/b/" = Error (err_prefix ^ {|too many '/'s in "/a/b/"|})
 
     let of_string text =
       let open Result.Syntax in
       let* rex, flags = split_substitution text in
-      if all_chars_in allowed_flags flags then
-        Error (Printf.sprintf "%s: invalid flags \"%s\" in \"%s\"" myname flags text)
-      else
-        let caseless = String.contains flags 'i' in
-        let* rex =
-          try
-            let flags =
-              if caseless then
-                [`CASELESS; `UTF]
-              else
-                [`UTF] in
-            Ok (Pcre2.regexp ~flags rex)
-          with
-          | e -> Error (Printf.sprintf "\"%s\": %s" text (Printexc.to_string e)) in
-        Ok { rex; text }
+      let* rex =
+        try
+          let err_postfix = {| in "|} ^ text ^ {|"|} in
+          let* flags = flags_of_string ~err_prefix ~err_postfix flag_map flags in
+          Ok (Pcre2.regexp ~flags:(`UTF :: flags) rex)
+        with
+        | e -> Error (Printf.sprintf "\"%s\": %s" text (Printexc.to_string e)) in
+      Ok { rex; text }
 
     let exec { rex; text } s =
       ignore text;
@@ -91,14 +88,17 @@ module M =
     let%test _ = exec' "/a/i" "AA" = Ok true
     let%test _ = exec' "|ab|" "aa" = Ok false
     let%test _ = exec' "|ab|" "aab" = Ok true
-    let%test _ = exec' "/a/x" "Aa" = Error "Perl.M: invalid flags \"x\" in \"/a/x\""
+    let%test _ = exec' "| a b|" "aab" = Ok false
+    let%test _ = exec' "| a b|x" "aab" = Ok true
+    let%test _ = exec' "/a/I" "Aa" = Error (err_prefix ^ {|invalid flag 'I' in "/a/I"|})
 
   end
 
 module S =
   struct
 
-    let allowed_flags = cset_of_string "ig"
+    let flag_map =
+      CMap.of_list [('i', `CASELESS); ('x', `EXTENDED); ('g', `UTF)] (* That's a HACK! *)
 
     type t =
       { rex : Pcre2.regexp;
@@ -110,6 +110,7 @@ module S =
       p.text
 
     let myname = "Perl.S"
+    let err_prefix = myname ^ ": "
 
     let split_substitution s =
       let l = String.length s in
@@ -124,30 +125,23 @@ module S =
         | [rex; sub; flags] -> Ok (rex, sub, flags)
         | _ -> Error (Printf.sprintf "%s: too many '%c's in \"%s\"" myname delim s)
 
-    let%test _ = split_substitution "/ab" = Error ("Perl.S: missing second '/' in \"/ab\"")
-    let%test _ = split_substitution "/a/b" = Error ("Perl.S: missing third '/' in \"/a/b\"")
+    let%test _ = split_substitution "/ab" = Error (err_prefix ^ {|missing second '/' in "/ab"|})
+    let%test _ = split_substitution "/a/b" = Error (err_prefix ^ {|missing third '/' in "/a/b"|})
     let%test _ = split_substitution "/a/b/" = Ok ("a", "b", "")
     let%test _ = split_substitution "/a//" = Ok ("a", "", "")
     let%test _ = split_substitution "/a/b/c" = Ok ("a", "b", "c")
     let%test _ = split_substitution "/a//c" = Ok ("a", "", "c")
-    let%test _ = split_substitution "/a/b/c/" = Error ("Perl.S: too many '/'s in \"/a/b/c/\"")
+    let%test _ = split_substitution "/a/b/c/" = Error (err_prefix ^ {|too many '/'s in "/a/b/c/"|})
 
     let of_string text =
       let open Result.Syntax in
       let* rex, sub, flags = split_substitution text in
-      if all_chars_in allowed_flags flags then
-        Error (Printf.sprintf "%s: invalid flags \"%s\" in \"%s\"" myname flags text)
-      else
-        let global = String.contains flags 'g'
-        and caseless = String.contains flags 'i' in
-        let* rex =
-          try
-            let flags =
-              if caseless then
-                [`CASELESS; `UTF]
-              else
-                [`UTF] in
-            Ok (Pcre2.regexp ~flags rex)
+      let global = String.contains flags 'g' in
+      let* rex =
+        try
+          let err_postfix = {| in "|} ^ text ^ {|"|} in
+          let* flags = flags_of_string ~err_prefix ~err_postfix flag_map flags in
+            Ok (Pcre2.regexp ~flags:(`UTF :: flags) rex)
           with
           | e -> Error (Printf.sprintf "\"%s\": %s" text (Printexc.to_string e))
         and* sub =
@@ -174,11 +168,15 @@ module S =
 
     let%test _ = exec' "/a/b/" "aa" = Ok "ba"
     let%test _ = exec' "/a/b/" "Aa" = Ok "Ab"
+    let%test _ = exec' "/ a /b/" "aa" = Ok "aa"
+    let%test _ = exec' "/ a /b/x" "aa" = Ok "ba"
+    let%test _ = exec' "/ a /b/xg" "aa" = Ok "bb"
+    let%test _ = exec' "/ a /b/xgi" "AA" = Ok "bb"
     let%test _ = exec' "/a/b/i" "Aa" = Ok "ba"
     let%test _ = exec' "/a/b/g" "aa" = Ok "bb"
     let%test _ = exec' "|a|b|g" "aa" = Ok "bb"
     let%test _ = exec' "/a/b/ig" "Aa" = Ok "bb"
-    let%test _ = exec' "/a/b/x" "Aa" = Error "Perl.S: invalid flags \"x\" in \"/a/b/x\""
+    let%test _ = exec' "/a/b/I" "Aa" = Error (err_prefix ^ {|invalid flag 'I' in "/a/b/I"|})
     let%test _ = exec' "/[Дт]/X/g" "Дмитрий" = Ok "XмиXрий"
 
   end
