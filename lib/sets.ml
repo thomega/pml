a62b6f89a00c306f56e8dca7ca5f6db0b9078c0e
Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
2026-02-10 17:40:44 +0100

    Sets.Integer

--- lib/sets.ml
+++ lib/sets.ml
@@ -20,3 +20,81 @@ module MBID = Set.Make (String)
 let mbid_union sets =
   List.fold_left MBID.union MBID.empty sets
 
+module Integers =
+  struct
+
+    module S = Set.Make (Int)
+
+    type range = { first : int; last : int }
+
+    let of_range { first; last } =
+      if first > last then
+        S.empty
+      else
+        let rec add set i =
+          if i > last then
+            set
+          else
+            add (S.add i set) (succ i) in
+        add (S.singleton first) (succ first)
+
+    let of_ranges ranges =
+      List.map of_range ranges |> List.fold_left S.union S.empty
+
+    let%test _ = of_ranges [{first = 5; last = 4 }] |> S.elements = []
+    let%test _ = of_ranges [{first = 4; last = 4 }] |> S.elements = [4]
+    let%test _ = of_ranges [{first = 4; last = 7 }] |> S.elements = [4;5;6;7]
+    let%test _ = of_ranges [{first = 4; last = 7 }; {first = 7; last = 8} ] |> S.elements = [4;5;6;7;8]
+
+    let parse_int s =
+      try
+        Ok (int_of_string s)
+      with
+      | e -> Error (Printexc.to_string e)
+
+    let range_of_string s =
+      let open Result.Syntax in
+      match String.split_on_char '-' s with
+      | [] -> assert false
+      | [""] -> Ok { first = 0; last = -1 }
+      | [first] ->
+         let* first = parse_int first in
+         Ok { first; last = first }
+      | [first;last] ->
+         let* first = parse_int first
+         and* last = parse_int last in
+         Ok { first; last }
+      | _ -> Error "too many '-'s in integer range"
+
+    let ranges_of_string s =
+      String.split_on_char ',' s |> Result_list.map range_of_string
+
+    let of_string s =
+      let open Result.Syntax in
+      let* ranges = ranges_of_string s in
+      Ok (of_ranges ranges)
+      
+    let%test_module _ =
+      (module struct
+
+         let expect s ilist =
+           let open Result.Syntax in
+           match
+             let* set = of_string s in
+             Ok (S.elements set = List.sort Int.compare ilist)
+           with
+           | Error msg -> prerr_endline msg; false
+           | Ok tof -> tof
+
+         let%test _ = expect "" []
+         let%test _ = expect "13" [13]
+         let%test _ = expect "13,18" [13; 18]
+         let%test _ = expect "13-18" [13; 14; 15; 16; 17; 18]
+         let%test _ = expect "18-13" []
+         let%test _ = expect "9,13-15,7" [7; 9; 13; 14; 15]
+         let%test _ = expect "7-9,13-15" [7; 8; 9; 13; 14; 15]
+         let%test _ = expect "7-9,8-10" [7; 8; 9; 10]
+
+       end)
+
+  end
--- lib/sets.mli
+++ lib/sets.mli
@@ -15,9 +15,20 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <https://www.gnu.org/licenses/>. *)
 
-(** Ubiquitous sets of MBID (i.e. UUID) strings *)
+(** Sets of things. *)
 
 module MBID : Set.S with type elt = string
+(** Ubiquitous sets of MBID (i.e. UUID) strings *)
 
 val mbid_union : MBID.t list -> MBID.t
 (** Shorthand for the union of a list of sets. *)
+
+module Integers : sig
+
+  module S : Set.S with type elt = int
+
+  val of_string : string -> (S.t, string) result
+  (** Parse expressions like [1,3-5,7]. *)
+
+end
+(** Integers. *)
